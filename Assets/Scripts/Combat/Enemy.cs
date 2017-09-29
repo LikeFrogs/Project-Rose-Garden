54a367a19684b5a726c6a839ebfbe3dbd558df71
Davis Snider <davissnider42@hotmail.com>
2017-09-28 19:12:07 -0700

    super basic classes and interface added

---
+++ Assets/Scripts/Combat/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ICombatChar
+{
+    #region Instance data
+    protected int health;
+    protected int maxHealth;
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Enemy's current health
+    /// </summary>
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+    /// <summary>
+    /// Enemy's max health
+    /// </summary>
+    public int MaxHealth
+    {
+        get { return maxHealth; }
+        set { maxHealth = value; }
+    }
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        //default testing values
+        health = 10;
+        maxHealth = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Calculates the initiative of the enemy
+    /// </summary>
+    /// <returns>The calculated initiative value</returns>
+    public int GetInitiative()
+    {
+        //determine the enemy's initiative
+        //we could just do a set value per enemy instance
+
+        //for testing purposes returns 0 (1 less than playable characters)
+        return 0;
+    }
+}
---
+++ Assets/Scripts/Combat/PlayableChar.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableChar : MonoBehaviour, ICombatChar
+{
+    #region Instance data
+    protected int health;
+    protected int maxHealth;
+    protected int strength;
+    protected int dexterity;
+    protected int intelligence;
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Character's current health
+    /// </summary>
+    public int Health
+    {
+        get { return health; }
+        set {  health = value; }
+    }
+    /// <summary>
+    /// Character's maximum health
+    /// </summary>
+    public int MaxHealth
+    {
+        get { return maxHealth; }
+        set { maxHealth = value; }
+    }
+    /// <summary>
+    /// Character's strength. Used for physical damage
+    /// </summary>
+    public int Strength
+    {
+        get { return strength; }
+        set { strength = value; }
+    }
+    /// <summary>
+    /// Character's dexterity. Used for speed and initiative
+    /// </summary>
+    public int Dexterity
+    {
+        get { return dexterity; }
+        set { dexterity = value; }
+    }
+    /// <summary>
+    /// Characters intelligence. Used for magic damage
+    /// </summary>
+    public int Intelligence
+    {
+        get { return intelligence; }
+        set { intelligence = value; }
+    }
+    #endregion
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        //default testing values
+        health = 10;
+        maxHealth = 10;
+        strength = 12;
+        dexterity = 12;
+        intelligence = 12;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    /// <summary>
+    /// Calculates the initiative of the character
+    /// </summary>
+    /// <returns>The calculated initiative value</returns>
+    public int GetInitiative()
+    {
+        //do some stuff to get an initiative value
+
+
+        //returns 1 right now to ensure that a value is returned and that 
+        //the value is higher than any test enemies (they will get 0)
+        return 1;
+    }
+}
