2ac24a89fab5f1cf5ab5ff0471838889fcb5f796
Chris Vantine <cav9225@rit.edu>
2017-09-30 00:36:46 -0400

    Auto stash before merge of "dev" and "origin/dev"

---
+++ Assets/Scripts/Combat/TurnHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TurnHandler : ScriptableObject
+{
+    /// <summary>
+    /// Initializes a character in the scene
+    /// </summary>
+    /// <param name="character"></param>
+    public void InitCharacter(CombatChar character)
+    {
+
+    }
+
+    /// <summary>
+    /// Sorts the a list of CombatChar objects by initiative
+    /// </summary>
+    public void SortInitiative()
+    {
+        
+    }
+}
